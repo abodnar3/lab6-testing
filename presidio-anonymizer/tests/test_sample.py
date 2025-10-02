95982fd2e86b7f7a4dea08c12de159de3f6f23fd
Andrew Bodnar <abodnar3@users.noreply.githubg.com>
2025-10-02 00:23:30 +0000

    Added test for 'sample.py '

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,5 +2,17 @@ import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
+    text = "My name is Bond."
+    start = 11
+    end = 15
+
+    result = sample_run_anonymizer(text, start, end)
+
+    assert isinstance(result, dict)
+    assert "text" in result
+    assert "items" in result
+    assert isinstance(result["items"], list)
+
+def test_sample_run_anonymizer():
     # replace the following line with your test
     pass
